70e25cfc57d2c61b64af9a125b4281db944a990a
MIGUEL ANICETO DOS SANTOS <miguel.asantos6@senacsp.edu.br>
2024-08-22 20:58:54 -0300

    add logo

--- entrega1-pi/src/components/Title.js
+++ entrega1-pi/src/components/Title.js
@@ -6,7 +6,7 @@ function Title(){
             
             <Header>
                 
-                <img src="https://via.placeholder.com/100" alt='logoEmpresa'/>
+                <img src="https://th.bing.com/th/id/R.047b032e66f967ea6ad96d83c0e69908?rik=TjTPvq0eEJUC7A&pid=ImgRaw&r=0" width='100px' alt='logoEmpresa'/>
                 <div>
                     <h1>Produtos</h1>
                 </div>
